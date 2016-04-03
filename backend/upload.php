cba4db2fe1a3bf5777202f1a503a66950fac2ef5
Gideon <gideonb234@gmail.com>
2016-04-03 04:02:27 +0100

    file upload works

--- backend/upload.php
+++ backend/upload.php
@@ -3,14 +3,14 @@ include_once('../error-enable.php');
 include_once('images.php');
 session_start();
 $id = $_SESSION['user_id'];
-if (isset($_POST["user_image"])) {
+if (isset($_POST["image"])) {
 	if (!empty($_FILES['user_image'])){
 	    $fileType = $_FILES["user_image"]["type"]; 
 	    if (($fileType == "image/gif") || ($fileType == "image/jpeg") ||
     	($fileType == "image/jpg") || ($fileType == "image/png")) {
     		//Replaces spaces in the name of the file with _ 
     		$file = preg_replace('/\s+/', '_', basename( $_FILES['user_image']['name']));
-    		$savedirectory = "upload/".$file;
+    		$savedirectory = "../upload/".$file;
     		move_uploaded_file($_FILES["user_image"]["tmp_name"], $savedirectory);
     		// $imagecontroller = new ImageHandler;
     		// $result = $imagecontroller->UploadImage($id, $file); 
