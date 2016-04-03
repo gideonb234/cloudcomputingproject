1c39129a652b9ff54f5ebea8245ab88da2c3efab
wkornatowski <masteremailvk@gmail.com>
2016-04-03 15:52:18 +0100

    index grid

--- index.php
+++ index.php
@@ -28,13 +28,31 @@
     <div class="row">
       <div class="large-12 columns">
         <div class="callout large">     
-          <div id="dropbox">
-			<span class="message">
-           <!-- uhh hope this works <3 -->
-   			 <h2>Hi! Please register or login to start uploading.</h2>
-		
-            </span>
-				</div>
+          <div id="imagebox">
+       
+       <!-- if all images arent populated, leave blank.gif so there is no fucking border....... like below.--> 
+       
+       <?php
+        require_once('backend/images.php');
+        $images = new ImageHandler();
+        $uid = $_SESSION['user_id'];
+        $listOfImages = $images->listImages($uid);
+        $count = 20;
+        foreach($listOfImages as $image){
+          $string = 'https://storage.googleapis.com/cloud-computing-storage/'.$image[image_filepath];
+          echo '
+          <div class="floated_img" style="height:10px; width:10px;">
+            <a href="image.php?image_id=' . $image[image_id].'"><img class="resize" src="'.$string.'" alt="img">
+        </div>';
+          $count--;
+        } 
+        for ($i=0; $i < $count; $i++) { 
+          echo '<div class="floated_img" style="height:10px; width:10px;">
+            <a href="http://goo.gl/ytbJn8"><img class="resize" src="http://goo.gl/ytbJn8" alt="img">
+        </div>';
+        }
+       ?>               
+          </div>
           </div>
           
         </div>
